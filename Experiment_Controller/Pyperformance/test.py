28023a3cf1d0e59cbbe4aa6751bd22b43d928a04
OnePabs <juanpablocontreras1313@gmail.com>
2022-03-04 04:04:40 -0500

    working on periods number of jobs per batch

--- Experiment_Controller/Pyperformance/test.py
+++ Experiment_Controller/Pyperformance/test.py
@@ -1,28 +1,24 @@
 from PerformanceWriter import PerformanceWriter
 
-listOfMeasurementFolders = [
-    "C:\\Users\\juanp\\Documents\\experiment_results\\B-exp-periods\\B-appIdx-0apiIdx-0rt-2700000st-40p-40ia-50\\measurements",
-    "C:\\Users\\juanp\\Documents\\experiment_results\\B-exp-periods\\B-appIdx-0apiIdx-0rt-2700000st-40p-50ia-50\\measurements",
-    "C:\\Users\\juanp\\Documents\\experiment_results\\B-exp-periods\\B-appIdx-0apiIdx-0rt-2700000st-40p-250ia-50\\measurements",
-    "C:\\Users\\juanp\\Documents\\experiment_results\\B-exp-periods\\B-appIdx-0apiIdx-0rt-2700000st-40p-1500ia-50\\measurements"
-]
 
-### Compute Cloud parameters ###
-entryMeasurementNameHandler = "STORAGE_API_ENTRY"
-serviceTimeStartMeasurementNameHandler = "ENTRY_LIST_EXIT"
-exitMeasurementNameHandler = "TRANSMITTER_EXIT"
-resultsPathHandler = "C:\\Users\\juanp\\Documents\\experiment_results\\resultsHandler.txt"
+# expPaths = [
+# "C:\\Users\\juanp\\Downloads\\drive-download-20220303T155216Z-001\\A-exp-saturation",
+# "C:\\Users\\juanp\\Downloads\\drive-download-20220303T155216Z-001\\B-exp-saturation",
+# "C:\\Users\\juanp\\Downloads\\drive-download-20220303T155216Z-001\\C-exp-saturation"
+# ]
 
-### Storage Cloud Parameters ###
-entryMeasurementNameManager = "ENTRY"
-serviceTimeStartMeasurementNameManager = "QueueExitTime"
-exitMeasurementNameManager = "EXIT"
-resultsPathManager = "C:\\Users\\juanp\\Documents\\experiment_results\\resultsManager.txt"
 
-for path in listOfMeasurementFolders:
-    print(path)
-    print("Compute Cloud")
-    PerformanceWriter.findAndWritePerformanceMetrics(path+"\\strgapi.txt",entryMeasurementNameHandler,serviceTimeStartMeasurementNameHandler,exitMeasurementNameHandler,resultsPathHandler)
-    print("Storage cloud")
-    PerformanceWriter.findAndWritePerformanceMetrics(path+"\\strgMngr.txt",entryMeasurementNameManager,serviceTimeStartMeasurementNameManager,exitMeasurementNameManager,resultsPathManager)
-    print()
+# resPaths = [
+# "C:\\Users\\juanp\\Downloads\\drive-download-20220303T155216Z-001\\A-results",
+# "C:\\Users\\juanp\\Downloads\\drive-download-20220303T155216Z-001\\B-results",
+# "C:\\Users\\juanp\\Downloads\\drive-download-20220303T155216Z-001\\C-results"
+# ]
+
+# numExperiments = len(expPaths)
+
+# for i in range(numExperiments):
+#     PerformanceWriter.findAndWritePerformanceMetricsOfExperiment(expPaths[i],resPaths[i])
+
+
+path = "C:\\Users\\juanp\\Documents\\experiment_results\\old\\B-exp-periods\\B-appIdx-0apiIdx-0rt-2700000st-40p-5ia-50\\measurements\\strgMngr.txt"
+PerformanceWriter.getAverageNumberOfRequestsInABatch(path,50)
