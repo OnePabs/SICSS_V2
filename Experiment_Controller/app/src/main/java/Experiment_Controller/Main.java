fe736cd5adf10f8fe231b6eb3a28dc4a832607f8
OnePabs <juanpablocontreras1313@gmail.com>
2022-07-09 12:51:37 -0500

    ready for technique adaptive exp

--- Experiment_Controller/app/src/main/java/Experiment_Controller/Main.java
+++ Experiment_Controller/app/src/main/java/Experiment_Controller/Main.java
@@ -10,17 +10,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Experiment Controller");
-		String experimentName = "test0";
+		String experimentName = "adaptive";
 
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		LocalDateTime now = LocalDateTime.now();
 		experimentName = dtf.format(now) + "-" + experimentName;
 
 		String resultsFolderPath;
-		//resultsFolderPath = "/home/ubuntu/experiment_results/"; //linux
+		resultsFolderPath = "/home/ubuntu/experiment_results/"; //linux
 		//resultsFolderPath = "C:\\Users\\Juan Pablo Contreras\\Documents\\expresults\\results"; //laptop
 		//resultsFolderPath = "C:\\Users\\juanp\\Documents\\experiment_results\\measurements"; //desktop
-		resultsFolderPath = "C:\\Users\\juanp\\OneDrive\\Documents\\experiment_results\\"; //TUF
+		//resultsFolderPath = "C:\\Users\\juanp\\OneDrive\\Documents\\experiment_results\\"; //TUF
 
 		String resultsPath = resultsFolderPath + experimentName;
 
@@ -37,30 +37,30 @@ public class Main {
 
 		boolean isVerbose = false;
 		
-		//String application_location = "http://ec2-3-138-174-172.us-east-2.compute.amazonaws.com:80";
-		String application_location = "http://localhost:8000";
-		//String handler_location = "http://ec2-18-116-45-199.us-east-2.compute.amazonaws.com:80";
-		String handler_location = "http://localhost:8080";
-		//String manager_location = "http://ec2-18-217-25-51.us-east-2.compute.amazonaws.com:80";
-		String manager_location = "http://localhost:8090";
+		String application_location = "http://ec2-3-145-9-118.us-east-2.compute.amazonaws.com:80";
+		//String application_location = "http://localhost:8000";
+		String handler_location = "http://ec2-3-145-99-120.us-east-2.compute.amazonaws.com:80";
+		//String handler_location = "http://localhost:8080";
+		String manager_location = "http://ec2-18-118-99-159.us-east-2.compute.amazonaws.com:80";
+		//String manager_location = "http://localhost:8090";
 
 		//RUNTIMES
 		long minutes_to_millis = (long)60000;
-		long[] runtimes = {5*minutes_to_millis};
+		long[] runtimes = {90*minutes_to_millis};
 		double loadFactor = 0.5;
 
 		//ARRIVAL TIMES
 		int[] inter_arrival_times = {50};
 		String inter_arrival_times_distribution;
-		inter_arrival_times_distribution = "CONSTANT";
-		//inter_arrival_times_distribution = "GEOMETRIC";
+		//inter_arrival_times_distribution = "CONSTANT";
+		inter_arrival_times_distribution = "GEOMETRIC";
 
 
 		//SERVICE TIMES
 		int[] service_times = {40};
 		String service_times_distribution;
-		service_times_distribution = "CONSTANT";
-		//service_times_distribution = "EXPONENTIAL";
+		//service_times_distribution = "CONSTANT";
+		service_times_distribution = "EXPONENTIAL";
 		boolean useStep=false;
 
 
